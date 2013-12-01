e7f13886af7976d4dbdd54176d74f6417fb7d727
bjoerne <bjoerne@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-01 16:10:30 +0000

    Initial setup
    git-svn-id: https://plugins.svn.wordpress.org/public-post-preview-configurator/trunk@813241 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public-post-preview-configurator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Public Post Preview Configurator
+ * Plugin URI: http://www.bjoerne.com
+ * Version: 1.0.0
+ * Description: Enables you to configure 'public post preview' plugin with a user interface.
+ * Author: Björn Weinbrenner
+ * Author URI: http://www.bjoerne.com/
+ *
+ * License: GPLv2 or later
+ */
+
+include_once dirname(__FILE__).'/options.php';
+
+if (is_admin()) {
+	add_filter( 'ppp_nonce_life', 'ppp_configurator_nonce_life');
+	add_action('admin_init', 'ppp_configurator_register_setting');
+	add_action('admin_menu', 'ppp_configurator_add_options_page');
+}
+
+function ppp_configurator_register_setting() {
+	register_setting('ppp_configurator_group', 'ppp_configurator_expiration_hours');
+}
+
+function ppp_configurator_add_options_page() {
+	add_options_page(__('Public Post Preview Configurator'), __('Public Post Preview Configurator'), 'manage_options', basename(__FILE__), 'ppp_configurator_show_settings_page');
+}
+
+function ppp_configurator_nonce_life($nonce_life) {
+	$expiration_hours = (int) get_option('ppp_configurator_expiration_hours');
+	if ($expiration_hours && $expiration_hours > 0) {
+		return 60 * 60 * $expiration_hours;
+	}
+	return $nonce_life;
+}
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Public Post Preview Configurator ===
+Contributors: bjoerne
+Tags: public, post, preview, posts, configuration, settings
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XS98Y5ASSH5S4
+Requires at least: 3.5
+Tested up to: 3.7.1
+Stable tag: trunk
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Enables you to configure the 'public post preview' plugin with a user interface.
+
+== Description ==
+
+With this plugin it's possible to configure the expiration time of a link provided by the [Public Post Preview](http://wordpress.org/plugins/public-post-preview/) plugin.
+
+The plugin is developed on [Github](https://github.com/bjoerne2/public-post-preview-configurator). Feel free to fork the project or create pull requests. 
+
+== Installation ==
+
+1. Upload plugin to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Configure plugin (Settings -> Public Post Preview Configurator)
+
+== Screenshots ==
+
+1. Options
